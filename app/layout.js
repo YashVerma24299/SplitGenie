727daa4659bd85351c2d70611abff5e971807e49
Yash <ritikverma23@gmail.com>
2025-10-22 19:51:30 +0530

    Creating app layout

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import header from "@/components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter =Inter({subsets: ["latin"]});
 
 export const metadata = {
   title: "SlpitGenie",
@@ -22,9 +15,13 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="/logos/logo-s.png" size="any"/>
       </head>
+
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className}`}
       >
+
+        <Header/>
+        
         <main className="min-h-screen"> {children}</main>
       </body>
     </html>
