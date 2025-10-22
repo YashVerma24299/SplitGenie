de7f09ae925b899016276d68263634874db28d08
Yash <ritikverma23@gmail.com>
2025-10-22 19:54:22 +0530

    Creating app layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import header from "@/components/header";
+import Header from "@/components/header";
 
 const inter =Inter({subsets: ["latin"]});
 
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
       >
 
         <Header/>
-        
+
         <main className="min-h-screen"> {children}</main>
       </body>
     </html>
