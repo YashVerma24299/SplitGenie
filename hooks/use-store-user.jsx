dfd177c3b64f250bde53baec670bbe8afba5f89f
Yash <ritikverma23@gmail.com>
2025-10-25 17:57:36 +0530

    useStoreUser hook used to store authenticated users in Convex DB and handle auth state

--- hooks/use-store-user.jsx
+++ hooks/use-store-user.jsx
@@ -4,33 +4,44 @@ import { useEffect, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "../convex/_generated/api";
 
+// This hook makes sure that:
+// 1. When a user logs in using Clerk
+// 2. The same user is also stored in Convex DB (users table)
+// 3. And only after storing, the app considers them “fully authenticated”
+// This ensures Clerk auth + Convex DB stay in sync.
+// OVERALL
+// This hook ensures the logged-in user is saved into the Convex database before allowing the app to treat them as authenticated.
+
+
 export function useStoreUser() {
+  // isLoading = Convex still checking auth
+  // isAuthenticated = Convex knows user is logged in
   const { isLoading, isAuthenticated } = useConvexAuth();
+
+
   const { user } = useUser();
-  // When this state is set we know the server
-  // has stored the user.
+
+
+  // This stores the ID of the user from Convex DB and Until we store the user in DB, it stays null
   const [userId, setUserId] = useState(null);
+
+  
+  // Call the `storeUser` mutation function to store and the current user in the `users` table and return the `Id` value.
   const storeUser = useMutation(api.users.store);
-  // Call the `storeUser` mutation function to store
-  // the current user in the `users` table and return the `Id` value.
   useEffect(() => {
     // If the user is not logged in don't do anything
     if (!isAuthenticated) {
       return;
     }
-    // Store the user in the database.
-    // Recall that `storeUser` gets the user information via the `auth`
-    // object on the server. You don't need to pass anything manually here.
+    
     async function createUser() {
-      const id = await storeUser();
-      setUserId(id);
+      const id = await storeUser(); // Inserts user into Convex DB
+      setUserId(id); // Save returned ID in state
     }
     createUser();
-    return () => setUserId(null);
-    // Make sure the effect reruns if the user logs in with
-    // a different identity
+    return () => setUserId(null); // Cleanup on logout
   }, [isAuthenticated, storeUser, user?.id]);
-  // Combine the local state with the state from context
+
   return {
     isLoading: isLoading || (isAuthenticated && userId === null),
     isAuthenticated: isAuthenticated && userId !== null,
